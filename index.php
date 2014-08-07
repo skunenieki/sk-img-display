22945906c1ad2c88ca277593752e29546654486e
Martins Sipenko <martins.sipenko@gmail.com>
2014-08-07 14:06:25 +0300

    Added styling

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!doctype html>
 <html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
@@ -8,15 +7,43 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <style>
             html {
-                background: url('http://www.downloadgimp.net/wp-content/uploads/2012/03/skies.jpg') no-repeat center center fixed;
-                -webkit-background-size: cover;
-                -moz-background-size: cover;
-                -o-background-size: cover;
-                background-size: cover;
+                background: #000 url('http://www.downloadgimp.net/wp-content/uploads/2012/03/skies.jpg') no-repeat center center fixed;
             }
         </style>
     </head>
     <body>
+        <div class="augsa" style="
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 50px;
+            font-size: 40px;
+            color: white;
+            width: 100%;
+            background: rgba(0, 0, 0, 0.7);
+            font-family: Verdana;
+            padding: 10px;
+        ">
+            Suti foto ar #skunenieki uz twitter vai instagram.
+        </div>
+
+        <div class="apaksa" style="
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            height: 50px;
+            font-size: 40px;
+            color: white;
+            width: 100%;
+            background: rgba(0, 0, 0, 0.7);
+            font-family: Verdana;
+            padding: 10px;
+        ">
+            <img class="twlogo" rc="https://g.twimg.com/Twitter_logo_white.png" style="height: 30px; margin-right: 5px;">
+            <img class="iglogo" src="https://g.twimg.com/Twitter_logo_white.png" style="height: 40px; margin-right: 5px; display:none;">
+            <div class="username" style="display: inline;">skunenieki</div>
+        </div>
+
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
         <script src="//js.pusher.com/2.2/pusher.min.js"></script>
@@ -26,9 +53,16 @@
 
             channel.bind('display', function(data) {
                 console.log(data);
-                $('html').css('background-image','url("'+data.url+'")');
+                $('html').css('background-image','#000 url("'+data.url+'") no-repeat center center fixed');
+                $('.username').text(data.username);
+                if (data.source == 'twitter') {
+                    $('.iglogo').hide();
+                    $('.twlogo').show();
+                } else if (data.source == 'ig') {
+                    $('.twlogo').hide();
+                    $('.iglogo').show();
+                }
             });
-
         </script>
     </body>
 </html>
