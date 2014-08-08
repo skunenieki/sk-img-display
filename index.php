8bc8a67117203a21f224aa6d3f8bca9ca2acfe79
Martins Sipenko <martins.sipenko@gmail.com>
2014-08-08 13:20:19 +0300

    full screen image

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <style>
             html {
                 background: #000 url('http://www.downloadgimp.net/wp-content/uploads/2012/03/skies.jpg') no-repeat center center fixed;
+                background-size: auto 100%;
                 overflow: hidden;
             }
         </style>
