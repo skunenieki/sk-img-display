6f253a4e560502dc42fe37cd98e3baa3d95dabcf
Martins Sipenko <martins.sipenko@gmail.com>
2015-07-26 15:24:55 +0300

    Updated pusher app key

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
         <script src="//js.pusher.com/2.2/pusher.min.js"></script>
         <script>
-            var pusher = new Pusher('f43e185cfa3b060dd7fe');
+            var pusher = new Pusher('79df90077ef347d83e96');
             var channel = pusher.subscribe('sk-image-display');
 
             channel.bind('display', function(data) {
